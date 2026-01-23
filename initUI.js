f3c0843d06a2045a52dda4501912191c697d7834
flybuk <flybuk@icloud.com>
2026-01-23 10:00:36 +0200

    Модификация процесса инициализации UI

--- initUI.js
+++ initUI.js
@@ -1,15 +1,40 @@
 const targetCont = __flybuk.config.target
 
-const UI = `
+const mainBlock = `
 <div class="main-block">
-<h1>Главный блок</h1>
+<header>
+    <div class="row">
+        <span id="month-year"></span>
+    </div>
+    <div class="row">
+        <div class="block">
+            <span class="block__title">Доход</span>
+            <div class="block__subblock">
+                <span id="summ-and-currency" class="block__summ"></span>
+                <button>+</button>
+            </div>
+        </div>
+        <div class="block">
+            <span class="block__title">Оставшаяся сумма</span>
+            <span id="remaining_summ" class="block__summ"></span>
+        </div>
+    </div>
+    <div class="row">
+        <h2>Расходы</h2>
+    </div>
+    
+</header>
 </div>
-<div class="first-run">Введите сумму вашей заработной платы</div>
 `
 
-if (targetCont) { document.querySelector(targetCont).innerHTML = UI }
-else { document.body.innerHTML = UI }
+const ui = {
+    mainBlock
+}
 
+console.log(Object.values(ui).join())
+
+if (targetCont) { document.querySelector(targetCont).innerHTML = Object.values(ui).join() }
+else { document.body.innerHTML = Object.values(ui).join() + document.body.innerHTML }
 
 
 __flybuk.emit('init:after')
