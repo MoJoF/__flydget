08f5d8696ba852f13dfb0a97441e84fb239d0136
Max <50298184+MoJoF@users.noreply.github.com>
2026-01-27 15:30:12 +0200

    Update __flybuk.js

--- __flybuk.js
+++ __flybuk.js
@@ -4,7 +4,7 @@ const target = script.dataset.target
 const config = {
     debug: true,
     target,
-    path: "https://github.com/MoJoF/__flydget",
+    path: "https://github.com/MoJoF/__flydget/",
     version: "0.1"
 }
 
