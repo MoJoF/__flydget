2eb55787ec6b22901071542b20d810962d2d2723
flybuk <flybuk@icloud.com>
2026-04-16 16:20:22 +0300

    Добавил alerts

--- pluginManager.js
+++ pluginManager.js
@@ -1,6 +1,7 @@
 __flybuk.on('plugin:installed', (plugin) => {
     let plugins = __flybuk.getSettings().plugins
     plugins = [...plugins, { ...plugin, enabled: false }]
+    alert('Плагин успешно установлен')
     __flybuk.setSettings({ plugins })
 })
 
@@ -8,10 +9,11 @@ __flybuk.on('plugin:uninstall', (plugin) => {
     let pluginObject = __flybuk.getSettings().plugins.find(plug => plug.id === plugin.id)
     let plugins = __flybuk.getSettings().plugins
     plugins = plugins.filter(p => p.id !== pluginObject.id)
+    alert('Плагин успешно удалён')
     __flybuk.setSettings({ plugins })
 })
 
-__flybuk.on('plugin:activate', (plugin) => { })
+__flybuk.on('plugin:activate', (plugin) => { alert('Плагин успешно активирован') })
 
 __flybuk.on('plugin:deactivate', (plugin) => {
     let pluginObject = __flybuk.getSettings().plugins.find(plug => plug.id === plugin.id) // Ищем объект плагина в настройках
@@ -20,5 +22,6 @@ __flybuk.on('plugin:deactivate', (plugin) => {
     plugins = plugins.filter(plug => plug.id !== pluginObject.id) // Удаляем объект плагина, чтобы заменить другим (с выключенным состоянием)
     plugins = [...plugins, pluginObject] // Перезаписываем объект с плагином
     delete __flybuk.pluginsRegistry[plugin.id] // Стираем плагин из регистра
+    alert('Плагин успешно деактивирован')
     __flybuk.setSettings({ plugins })
 })
